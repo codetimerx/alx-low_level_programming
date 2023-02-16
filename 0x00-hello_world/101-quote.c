11888779b70852fd15cf89fe2acef6e62d2d7741
codetimerx <codetimerx@gmail.com>
2023-02-16 13:17:04 -0800

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<unistd.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, message, 39);
+  return 1;
+}
