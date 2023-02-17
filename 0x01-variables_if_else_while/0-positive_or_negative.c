64bfc34777ef46e648b21b4c5fb6956604229f68
codetimerx <codetimerx@gmail.com>
2023-02-17 13:31:51 -0800

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,16 +9,18 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf(" is positive\n ");
+	printf(" is positive\n");
 	} else 
 	if (n < 0)
 	{
-	printf(" is negative\n ");
-	} else
+	printf(" is negative\n");
+	}
+       	else
 	{
 	printf(" is zero\n ");
 	}
