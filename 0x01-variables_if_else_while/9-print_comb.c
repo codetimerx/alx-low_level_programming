7992c02e42548cbee19e043c9c82c8f262227b28
codetimerx <codetimerx@gmail.com>
2023-02-17 16:36:57 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,40 +7,17 @@
  */
 int main(void)
 {
-int i, j, k, l;
+int i;
 
 for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
-putchar(',');
-putchar(' ');
-
-for (j = i + 1; j < 10; j++)
-{
-putchar(j + '0');
-putchar(',');
-putchar(' ');
-
-for (k = j + 1; k < 10; k++)
-{
-putchar(k + '0');
-putchar(',');
-putchar(' ');
-
-for (l = k + 1; l < 10; l++)
-{
-putchar(l + '0');
-
-if (i != 9 || j != 8 || k != 7 || l != 6)
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
-}
-}
-putchar('\n');
 
 return (0);
 }
