3cd1a039011c27d877291b394169fa55c4970c9b
codetimerx <codetimerx@gmail.com>
2023-02-21 14:43:31 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *
  * @n: The number to check.
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, and -1 if n is less than zero.
+ * Return: 1 if n is > than zero, 0 if n is zero, and -1 if n is < than zero.
  */
 int print_sign(int n)
 {
