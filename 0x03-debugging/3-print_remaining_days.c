07c2664045841c57da403353543a7617b8d12b42
codetimerx <codetimerx@gmail.com>
2023-02-23 02:29:45 -0800

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,27 +11,25 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
+int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int is_leap_year = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+int day_of_year = 0;
+int i;
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+
+for (i = 0; i < month - 1; i++)
+{
+day_of_year += days_in_month[i];
+}
+day_of_year += day;
+
+
+if (is_leap_year && month > 2)
+{
+day_of_year++;
+}
+
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", (is_leap_year ? 366 : 365) - day_of_year);
 }
 
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0 (Success)
  */
-int positive_or_negative(void)
+int positive_or_negative()
 {
 int n;
 
