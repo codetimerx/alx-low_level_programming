badde71d20c7c03f7e0dc6fb4e5070cd05dbef71
codetimerx <codetimerx@gmail.com>
2023-02-23 12:15:55 -0800

    unction that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @y: first integer
+* @x: second integer
+* Return: addition of y and x
+*/
+int mul(int y, int x)
+{
+return (y * x);
+}
