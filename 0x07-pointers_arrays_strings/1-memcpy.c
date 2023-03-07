e2bcdd8263e16d5dba50f0c5823b8cdc2e6c65e5
codetimerx <codetimerx@gmail.com>
2023-03-06 17:52:20 -0800

    Implement a memcopy function using pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - Copies n bytes from memory area src to memory area dest
+ *
+ * @dest: Pointer to the destination memory area
+ * @src: Pointer to the source memory area
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *ptr = dest;
+
+while (n-- > 0)
+{
+*ptr++ = *src++;
+}
+
+return (dest);
+}
+
