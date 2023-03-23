ae9ac5f6eb2aa853737f1f7706c19f81c33804ab
codetimerx <codetimerx@gmail.com>
2023-03-23 14:53:18 -0700

    A func that prints numbers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,51 +1,53 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
- * print_all - Prints any combination of char, integer, float, and string
- * @format: List of types of arguments passed to the function
- * c: char
- * i: integer
- * f: float
- * s: char * (if the string is NULL, print (nil) instead)
+ * print_all - Prints all of the arguments when specified
+ * @format: specifies the necessary operations
+ * Return: void
  */
+
 void print_all(const char * const format, ...)
 {
-va_list args;
-const char *f;
-char cval;
-int ival;
-double dval;
-char *sval;
-va_start(args, format);
-for (f = format; *f != '\0'; ++f)
-{
-if (*f == 'c')
-{
-cval = (char)va_arg(args, int);
-printf("%c", cval);
-}
-else if (*f == 'i')
-{
-ival = va_arg(args, int);
-printf("%d", ival);
-}
-else if (*f == 'f')
-{
-dval = va_arg(args, double);
-printf("%f", dval);
-}
-else if (*f == 's')
-{
-sval = va_arg(args, char *);
-if (sval == NULL)
-printf("(nil)");
-else
-printf("%s", sval);
-}
-if (*(f + 1) != '\0' && (*f == 'c' || *f == 'i' || *f == 'f' || *f == 's'))
-printf(", ");
-}
-va_end(args);
-printf("\n");
-}
+	int i;
+	int flag;
+	char *str;
+	va_list a_list;
 
+	va_start(a_list, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(a_list);
+}
